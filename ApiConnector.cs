e86a300f837a275a7989fb7c7d924a8fed34d7f8
Maks Sieminski <maxsieminski@gmail.com>
2020-12-08 14:54:36 +0100

    template for api connection

---
+++ ApiConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ErgastApi.Client;
+using ErgastApi.Ids;
+using ErgastApi.Requests;
+using ErgastApi.Responses;
+
+namespace TCP_Server_Asynchronous
+{
+    abstract class ApiConnector
+    {
+        private static ErgastClient client;
+        private static ErgastResponse response;
+        private static ErgastRequest<ErgastResponse> request;
+
+        public static async Task GetRequest(string category, string[] args)
+        {
+            
+        }
+    }
+}
